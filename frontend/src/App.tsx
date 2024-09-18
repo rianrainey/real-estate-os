e7c2f1d4a278d86cf8e49d6fb2aab72c7dba6932
Rian Rainey <rianrainey@gmail.com>
2024-09-18 14:24:07 -0400

    Dynamically submit form email and password

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,35 +2,68 @@ import { FormEvent, useState } from 'react'
 import './App.css'
 import axios from 'axios'
 
-interface LoginReponse {
-  token: string
-}
+export const Login = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
 
-// FormEvent is a React Synthetic event that is of type HTMLFormElement
-// HTMLFormElement is a JS interface that represents a form element in the DOM
-const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-  // Prevent refreshing of page which is default browser behavior
-  e.preventDefault();
+  interface LoginReponse {
+    token: string
+  }
 
-  // axios post to url
-  const port = '3000'
-  const url = `http://localhost:${port}/login`
-  const payload = {
-    user: {
-      email: 'test1@acme.com',
-      password: '123456'
+  // FormEvent is a React Synthetic event that is of type HTMLFormElement
+  // HTMLFormElement is a JS interface that represents a form element in the DOM
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    // Prevent refreshing of page which is default browser behavior
+    e.preventDefault();
+
+    // axios post to url
+    const port = '3000'
+    const url = `http://localhost:${port}/login`
+
+    // If no email or password, return
+    if (!email || !password) {
+      console.error('Email and Password are required')
+      return
+    }
+
+    const payload = {
+      user: {
+        email: email,
+        password: password
+      }
+    }
+
+    try {
+      // axios.post will return a promise
+      // of type: Promise<AxiosReponse<LoginResponse, any>>
+      const response = await axios.post<LoginReponse>(url, payload)
+      localStorage.setItem('token', response.headers.authorization)
+    } catch (error) {
+      console.error('Error loggin in', error);
     }
   }
 
-  try {
-    // axios.post will return a promise
-    // of type: Promise<AxiosReponse<LoginResponse, any>>
-    const response = await axios.post<LoginReponse>(url, payload)
-    localStorage.setItem('token', response.headers.authorization)
-  } catch (error) {
-    console.error('Error loggin in', error);
-
-  }
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          value={email}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
+          placeholder="Email"
+          required
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
+          placeholder="Password"
+          required
+        />
+        <button type="submit">Login</button>
+      </form>
+    </>
+  )
 }
 
 function App() {
@@ -40,21 +73,17 @@ function App() {
     <>
       <h1>Real Estate OS</h1>
       <div className="card">
-        {/* // This works by the button html element taking an onClick function that is defined inline.
+        {/*
+        // This works by the button html element taking an onClick function that is defined inline.
         // It is calling the useState setCount method defined above that sets the count variable.
         // It takes the current count as a parameter and returns it + 1.
-        // When count is re-rendered, it will show the new value */}
+        // When count is re-rendered, it will show the new value
+        */}
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <Login />
 
-        <form onSubmit={handleSubmit}>
-          <label>Email</label>
-          <input type='email' placeholder='elon@tesla.com' required />
-          <label>Password</label>
-          <input type='password' required />
-          <button type='submit'>Login</button>
-        </form>
       </div>
     </>
   )
