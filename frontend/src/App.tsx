73e3389e181fa804e8f192dedd550fe897a4d1a2
Rian Rainey <rianrainey@gmail.com>
2024-09-18 12:16:09 -0400

    Successfully login manually

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { FormEvent, useState } from 'react'
 import './App.css'
+import axios from 'axios'
+
+interface LoginReponse {
+  token: string
+}
+
+// FormEvent is a React Synthetic event that is of type HTMLFormElement
+// HTMLFormElement is a JS interface that represents a form element in the DOM
+const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+  // Prevent refreshing of page which is default browser behavior
+  e.preventDefault();
+
+  // axios post to url
+  const port = '3000'
+  const url = `http://localhost:${port}/login`
+  const payload = {
+    user: {
+      email: 'test1@acme.com',
+      password: '123456'
+    }
+  }
+
+  try {
+    // axios.post will return a promise
+    // of type: Promise<AxiosReponse<LoginResponse, any>>
+    const response = await axios.post<LoginReponse>(url, payload)
+    localStorage.setItem('token', response.headers.authorization)
+  } catch (error) {
+    console.error('Error loggin in', error);
+
+  }
+}
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Real Estate OS</h1>
       <div className="card">
+        {/* // This works by the button html element taking an onClick function that is defined inline.
+        // It is calling the useState setCount method defined above that sets the count variable.
+        // It takes the current count as a parameter and returns it + 1.
+        // When count is re-rendered, it will show the new value */}
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
+        <form onSubmit={handleSubmit}>
+          <label>Email</label>
+          <input type='email' placeholder='elon@tesla.com' required />
+          <label>Password</label>
+          <input type='password' required />
+          <button type='submit'>Login</button>
+        </form>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
